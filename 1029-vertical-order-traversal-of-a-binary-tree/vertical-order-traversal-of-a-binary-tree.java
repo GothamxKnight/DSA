9317796f5a4480b02e900fd10637917071f1f400
Naman Mahara <namansm323@gmail.com>
2026-07-30 13:07:32 +0530

    Time: 5 ms (13.26%) | Memory: 44.5 MB (10.08%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -13,49 +13,44 @@
  *     }
  * }
  */
-public class node {
-    TreeNode root;
-    int level;
-
-    node(int level) {
-        this.level = level;
-    }
-
-    node(TreeNode root, int level) {
-        this.root = root;
-        this.level = level;
-    }
-}
-
 class Solution {
+    public class node{
+        TreeNode root;
+        int level;
+        node(TreeNode root,int level){
+            this.root=root;
+            this.level=level;
+        }
+    }
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        node Root = new node(root, 0);
-        Queue<node> q = new ArrayDeque<>();
-        TreeMap<Integer, List<Integer>> map = new TreeMap<>();
-        q.add(Root);
-        while (!q.isEmpty()) {
-            int size = q.size();
-            TreeMap<Integer, List<Integer>> rowmap = new TreeMap<>();
-            for (int i = 0; i < size; i++) {
-                node temp = q.poll();
-                rowmap.putIfAbsent(temp.level, new ArrayList<>());
-                rowmap.get(temp.level).add(temp.root.val);
-                if (temp.root.left != null) {
-                    q.add(new node(temp.root.left, temp.level - 1));
+        List<List<Integer>> res=new ArrayList<>();
+        TreeMap<Integer,List<Integer>> map=new TreeMap<>();
+        Queue<node> qe=new ArrayDeque<>();
+        qe.add(new node(root,0));
+        while(!qe.isEmpty()){
+            int n=qe.size();
+            TreeMap<Integer,List<Integer>> temp_map=new TreeMap<>();
+            for(int i=0;i<n;i++){
+                node temp=qe.poll();
+                temp_map.putIfAbsent(temp.level,new ArrayList<>());
+                temp_map.get(temp.level).add(temp.root.val);
+                if(temp.root.left!=null){
+                    qe.add(new node(temp.root.left,temp.level-1));
                 }
-                if (temp.root.right != null) {
-                    q.add(new node(temp.root.right, temp.level + 1));
+                if(temp.root.right!=null){
+                    qe.add(new node(temp.root.right,temp.level+1));
                 }
             }
-            for (Map.Entry<Integer,List<Integer>> temp : rowmap.entrySet()) {
-                Collections.sort(temp.getValue());
-                map.putIfAbsent(temp.getKey(),new ArrayList<>());
-                for(Integer a:temp.getValue()){
-                    map.get(temp.getKey()).add(a);
+            for(Map.Entry<Integer,List<Integer>> mp:temp_map.entrySet()){
+                map.putIfAbsent(mp.getKey(),new ArrayList<>());
+                List<Integer> temp=new ArrayList<>(mp.getValue());
+                Collections.sort(temp);
+                for(Integer a:temp){
+                    map.get(mp.getKey()).add(a);
                 }
             }
         }
-        List<List<Integer>> list=new ArrayList<>(map.values());
-        return list;
+        res=new ArrayList<>(map.values());
+        return res;
     }
 }
